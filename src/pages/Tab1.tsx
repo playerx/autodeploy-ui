badbae0481f479d939b451be77a41506762f85da
Ezeki Zibzibadze <ez@jok.io>
2022-03-31 00:16:20 +0400

    fix: build issue

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -5,7 +5,6 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import { version } from "../version";
 import "./Tab1.css";
 
