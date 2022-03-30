849859cb342d592a5abafb232b0a9f694f0750c4
Ezeki Zibzibadze <ez@jok.io>
2022-03-31 00:06:52 +0400

    feat: set version

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -6,6 +6,7 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
+import { version } from "../version";
 import "./Tab1.css";
 
 const Tab1: React.FC = () => {
@@ -13,13 +14,13 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>პირველი ტაბი</IonTitle>
+          <IonTitle>ვერსია {version}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">{version.length}</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
